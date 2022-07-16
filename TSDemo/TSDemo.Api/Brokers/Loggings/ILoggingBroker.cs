b4e8f6070b740e307c667fbf5d91f496fa4c42e6
ahmed magdi <ahmed.magdi@bgomla.com>
2022-07-16 16:04:32 +0200

    BROKERS: Logging - Log LogError

--- TSDemo/TSDemo.Api/Brokers/Loggings/ILoggingBroker.cs
+++ TSDemo/TSDemo.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -8,5 +8,6 @@ namespace TSDemo.Api.Brokers.Loggings
         void LogTrace(string message);
         void LogDebug(string message);
         void LogWarning(string message);
+        void LogError(Exception exception);
     }
 }
