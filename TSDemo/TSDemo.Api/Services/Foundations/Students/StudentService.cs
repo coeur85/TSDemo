93745314e8345205ff18b991d9b1fd61e92fb59c
ahmed magdi <ahmed.magdi@bgomla.com>
2022-07-16 16:15:05 +0200

    ShouldReturnStudent -> PASS

--- TSDemo/TSDemo.Api/Services/Foundations/Students/StudentService.cs
+++ TSDemo/TSDemo.Api/Services/Foundations/Students/StudentService.cs
@@ -32,6 +32,6 @@ namespace TSDemo.Api.Services.Foundations.Students
             });
 
         public IQueryable<Student> RetrieveAllStudents() =>
-            throw new System.NotImplementedException();
+            this.storageBroker.SelectAllStudents();
     }
 }
