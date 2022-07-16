73a0444209feb2b5ee5e7fe8b801b2c058b18642
ahmed magdi <ahmed.magdi@bgomla.com>
2022-07-16 16:15:13 +0200

    ShouldRetrieveStudentByIdAsync -> FAIL

--- TSDemo/TSDemo.Api/Services/Foundations/Students/IStudentService.cs
+++ TSDemo/TSDemo.Api/Services/Foundations/Students/IStudentService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using TSDemo.Api.Models.Students;
@@ -8,5 +9,6 @@ namespace TSDemo.Api.Services.Foundations.Students
     {
         ValueTask<Student> AddStudentAsync(Student student);
         IQueryable<Student> RetrieveAllStudents();
+        ValueTask<Student> RetrieveStudentByIdAsync(Guid studentId);
     }
 }
