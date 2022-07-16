9c998745f7d442e7b8bc3bbf4a1f8f8533eaf10b
ahmed magdi <ahmed.magdi@bgomla.com>
2022-07-16 17:57:17 +0200

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

--- TSDemo/TSDemo.Api.Tests.Unit/Services/Foundations/Schools/SchoolServiceTests.Exceptions.RemoveById.cs
+++ TSDemo/TSDemo.Api.Tests.Unit/Services/Foundations/Schools/SchoolServiceTests.Exceptions.RemoveById.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using TSDemo.Api.Models.Schools;
 using TSDemo.Api.Models.Schools.Exceptions;
@@ -54,6 +56,55 @@ namespace TSDemo.Api.Tests.Unit.Services.Foundations.Schools
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someSchoolId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedSchoolException =
+                new LockedSchoolException(databaseUpdateConcurrencyException);
+
+            var expectedSchoolDependencyValidationException =
+                new SchoolDependencyValidationException(lockedSchoolException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSchoolByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<School> removeSchoolByIdTask =
+                this.schoolService.RemoveSchoolByIdAsync(someSchoolId);
+
+            SchoolDependencyValidationException actualSchoolDependencyValidationException =
+                await Assert.ThrowsAsync<SchoolDependencyValidationException>(
+                    removeSchoolByIdTask.AsTask);
+
+            // then
+            actualSchoolDependencyValidationException.Should()
+                .BeEquivalentTo(expectedSchoolDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSchoolByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSchoolDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteSchoolAsync(It.IsAny<School>()),
                     Times.Never);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
