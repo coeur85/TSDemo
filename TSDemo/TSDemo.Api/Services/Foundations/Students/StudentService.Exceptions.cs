bf5d8911d3dd9906c8b320232b7d7972645260fb
ahmed magdi <ahmed.magdi@bgomla.com>
2022-07-16 16:14:56 +0200

    ShouldThrowValidationExceptionOnAddIfReferenceErrorOccursAndLogItAsync -> PASS

--- TSDemo/TSDemo.Api/Services/Foundations/Students/StudentService.Exceptions.cs
+++ TSDemo/TSDemo.Api/Services/Foundations/Students/StudentService.Exceptions.cs
@@ -39,6 +39,13 @@ namespace TSDemo.Api.Services.Foundations.Students
 
                 throw CreateAndLogDependencyValidationException(alreadyExistsStudentException);
             }
+            catch (ForeignKeyConstraintConflictException foreignKeyConstraintConflictException)
+            {
+                var invalidStudentReferenceException =
+                    new InvalidStudentReferenceException(foreignKeyConstraintConflictException);
+
+                throw CreateAndLogDependencyValidationException(invalidStudentReferenceException);
+            }
         }
 
         private StudentValidationException CreateAndLogValidationException(Xeption exception)
