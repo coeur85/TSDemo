f6fb5b0f31a3e0ea60aab78aaffbbd9c901b7f34
ahmed magdi <ahmed.magdi@bgomla.com>
2022-07-16 17:57:05 +0200

    ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync -> PASS

--- TSDemo/TSDemo.Api.Tests.Unit/Services/Foundations/Schools/SchoolServiceTests.Exceptions.Modify.cs
+++ TSDemo/TSDemo.Api.Tests.Unit/Services/Foundations/Schools/SchoolServiceTests.Exceptions.Modify.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
 using Moq;
@@ -54,6 +55,60 @@ namespace TSDemo.Api.Tests.Unit.Services.Foundations.Schools
 
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateSchoolAsync(randomSchool),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync()
+        {
+            // given
+            School someSchool = CreateRandomSchool();
+            string randomMessage = GetRandomMessage();
+            string exceptionMessage = randomMessage;
+
+            var foreignKeyConstraintConflictException =
+                new ForeignKeyConstraintConflictException(exceptionMessage);
+
+            var invalidSchoolReferenceException =
+                new InvalidSchoolReferenceException(foreignKeyConstraintConflictException);
+
+            SchoolDependencyValidationException expectedSchoolDependencyValidationException =
+                new SchoolDependencyValidationException(invalidSchoolReferenceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(foreignKeyConstraintConflictException);
+
+            // when
+            ValueTask<School> modifySchoolTask =
+                this.schoolService.ModifySchoolAsync(someSchool);
+
+            SchoolDependencyValidationException actualSchoolDependencyValidationException =
+                await Assert.ThrowsAsync<SchoolDependencyValidationException>(
+                    modifySchoolTask.AsTask);
+
+            // then
+            actualSchoolDependencyValidationException.Should()
+                .BeEquivalentTo(expectedSchoolDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSchoolByIdAsync(someSchool.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedSchoolDependencyValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateSchoolAsync(someSchool),
                     Times.Never);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
